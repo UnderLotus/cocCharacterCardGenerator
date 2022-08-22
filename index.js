9d84be2d181bdaf5d71845c75575dd55acefd350
UnderLotus <84985759+UnderLotus@users.noreply.github.com>
2022-08-22 15:59:26 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs=require("fs"); 
+const { Agent } = require("http");
 
 function roll3d6(){
     let first = Math.floor(Math.random()*6)+1
@@ -57,30 +58,38 @@ function db(){
     return db
 }
 
-let finalStatus = {
-    '力量':status.STR,
-    '敏捷':status.DEX,
-    '意志':status.POW,
-    '體質':status.CON,
-    '外表':status.APP,
-    '體型':status.SIZ,
-    '智慧':status.INT,
-    '知識':KNOW,
-    '幸運':LUCK,
-    '靈感':IDEA,
-    '教育':status.EDU,
-    '傷害修正':db(),
-    'HP':HP,
-    'MP':MP,
-    'SAN':SAN,
-    '年齡':AGE,
-    '職業技能點數':jobPoint,
-    '個人技能點數':personaPoint
-}
+// let finalStatus = {
+//     '力量':status.STR,
+//     '敏捷':status.DEX,
+//     '意志':status.POW,
+//     '體質':status.CON,
+//     '外表':status.APP,
+//     '體型':status.SIZ,
+//     '智慧':status.INT,
+//     '知識':KNOW,
+//     '幸運':LUCK,
+//     '靈感':IDEA,
+//     '教育':status.EDU,
+//     '傷害修正':db(),
+//     'HP':HP,
+//     'MP':MP,
+//     'SAN':SAN,
+//     '年齡':AGE,
+//     '職業技能點數':jobPoint,
+//     '個人技能點數':personaPoint
+// }
+
+let finalStatusString = `力量：${status.STR} 敏捷：${status.DEX} 意志：${status.POW}
+體質：${status.CON} 外表：${status.APP} 智慧：${status.INT}
+體型：${status.SIZ} 知識：${KNOW} 幸運：${LUCK}
+靈感：${IDEA} 教育：${status.EDU} DB：${db()}
+HP：${HP} MP：${MP} SAN：${SAN} 年齡：${AGE}
+職業技能點數：${jobPoint} 個人技能點數：${personaPoint}`
 
 // console.log(finalStatus)
+// console.log(finalStatusString)
 
-fs.writeFile('./output.txt', JSON.stringify(finalStatus), function (err) {
+fs.writeFile('./output.txt', finalStatusString, function (err) {
     if (err)
         console.log(err);
     else
